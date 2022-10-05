44b9e17cc9abd6cd3f8e2204abb4543f4a5b55b0
nxoo <nxoo.dev@gmail.com>
2022-10-05 16:09:31 +0300

    added React Router v6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from "./components/navbar";
 import Polls from "./components/polls";
 import './App.css';
 
-function App() {
+export default function App() {
     return (
-        <>
-            <Navbar/>
-            <div className="container col-sm-9">
-                <Polls/>
-            </div>
-        </>
-    );
+        <Router>
+            <Routes>
+                <Route path='/' element={<Navbar/>}>
+                    <Route path='/' element={<Polls/>}/>
+                </Route>
+            </Routes>
+        </Router>
+    )
 }
-
-export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,36 @@
 import React from "react";
+import {Outlet} from "react-router-dom"
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand bg-light fs-5 mb-sm-5 mb-3">
-            <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                        <li className="nav-item mx-2">
-                            <a className="nav-link ps-0 active" aria-current="page" href="#">
-                                <i className="bi bi-house-door-fill"></i>{' '}
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <a className="nav-link active" aria-current="page" href="#">
-                                <i className="bi bi-file-earmark-plus"></i>{' '}
-                                Create Poll
-                            </a>
-                        </li>
-                    </ul>
+        <div>
+            <nav className="navbar navbar-expand bg-light fs-5 mb-sm-5 mb-3">
+                <div className="container">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item mx-2">
+                                <a className="nav-link ps-0 active" aria-current="page" href="/">
+                                    <i className="bi bi-house-door-fill"></i>{' '}
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item mx-2">
+                                <a className="nav-link active" aria-current="page" href="/new-poll">
+                                    <i className="bi bi-file-earmark-plus"></i>{' '}
+                                    Create Poll
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <Outlet/>
+        </div>
     )
 }
 
