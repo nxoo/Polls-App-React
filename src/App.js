dc8c94f9fa938d0fa017d3b1a96b235b149674a8
nxoo <nxoo.dev@gmail.com>
2022-10-06 11:49:01 +0300

    added new componets to Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navbar from "./components/navbar";
 import Polls from "./components/polls";
+import NewPoll from "./components/newPoll";
+import Vote from "./components/vote";
+import Results from "./components/results";
 import './App.css';
 
 export default function App() {
@@ -10,6 +13,9 @@ export default function App() {
             <Routes>
                 <Route path='/' element={<Navbar/>}>
                     <Route path='/' element={<Polls/>}/>
+                    <Route path='/newPoll' element={<NewPoll />}/>
+                    <Route path='/vote' element={<Vote/>}/>
+                    <Route path='/results' element={<Results/>}/>
                 </Route>
             </Routes>
         </Router>
